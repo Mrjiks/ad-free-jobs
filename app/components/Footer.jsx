e532d80e99a12213664e3acaf1ee250b959b535c
Harrison <jiksdevops@gmail.com>
2023-06-05 09:11:42 +0200

    Align footer contents vertically on mobile screens

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,9 +3,9 @@ import { BsBagFill } from "react-icons/bs";
 import Logo from "./atoms/Logo";
 const Footer = () => {
   return (
-    <div className='flex justify-between px-24 align-middle'>
+    <div className='flex flex-col justify-center px-24 my-10 align-middle md:justify-between md:flex-row lg:flex-row 2xl:flex-row lg:justify-between '>
       <Logo />
-      <p className='flex-1'>Coming Soon</p>
+      <p className='flex-1 '>Coming Soon</p>
     </div>
   );
 };
