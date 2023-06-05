c4a2243f57fda4d98d169b14df17e78bad92b74d
Harrison <jiksdevops@gmail.com>
2023-06-05 09:43:43 +0200

    Bind input value to component state

--- app/components/atoms/Input.jsx
+++ app/components/atoms/Input.jsx
@@ -1,9 +1,16 @@
+"use client";
 import React from "react";
 
-const Input = ({ className, type, placeholder }) => {
+const Input = ({ className, type, placeholder, autoFocus, onChange }) => {
   return (
     <div className='flex flex-col w-full gap-3 md:flex-row'>
-      <input type={type} placeholder={placeholder} className={className} />
+      <input
+        type={type}
+        placeholder={placeholder}
+        className={className}
+        autoFocus={autoFocus}
+        onChange={onChange}
+      />
     </div>
   );
 };
--- app/page.js
+++ app/page.js
@@ -1,22 +1,41 @@
+"use client";
 import Input from "./components/atoms/Input";
 import Button from "./components/atoms/Button";
+import { useState } from "react";
 
 export default function Home() {
+  const [searchTerm, setSearchTerm] = useState();
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchTerm(() => e.target.value);
+  };
   return (
     <main className='flex flex-col justify-between min-h-screen gap-2 '>
       <section className='flex flex-col justify-center flex-1 p-2 place-content-center bg-black-500'>
         <div className='flex justify-center mb-7 '>
-          <h1 className='text-2xl font-bold'>The Easiest Way to Get Your New Job</h1>
+          <h1 className='text-2xl font-bold md:text-5xl lg:text-5xl'>
+            The Easiest Way to Get Your New Job
+          </h1>
         </div>
-        <p className='mb-3 text-center'>Latest Ad-free job board</p>
+        <p className='mb-10 text-center md:text-3xl lg:text-3xl'>Latest Ad-free job board</p>
         <div className='flex justify-center'>
           <form className='flex flex-col justify-between gap-3 md:flex-row'>
             <Input
               placeholder='Job Title, Company name, or role'
               type='text'
-              className='p-2 rounded-sm'
+              className='p-2 text-black rounded-sm'
+              autoFocus='autoFocus'
+              value={searchTerm}
+              name='search-job'
+              onChange={handleChange}
             />
-            <Input placeholder='City, Province, or region' type='text' className='p-2 rounded-sm' />
+            <Input
+              placeholder='City, Province, or region'
+              type='text'
+              className='p-2 text-black rounded-sm'
+              name='search-job'
+              onChange={handleChange}
+            />
 
             <div className='flex flex-col w-full gap-3 md:flex-row'>
               <Button
@@ -28,10 +47,14 @@ export default function Home() {
           </form>
         </div>
       </section>
-      <section className='bg-white flex-2'>
+      <section className='py-10 bg-white flex-2'>
         <div className='flex flex-col justify-center'>
-          <p className='text-center text-black'>Popular Categories</p>
-          <p className='mb-3 text-center text-black'>Latest Ad-free job board</p>
+          <h2 className='font-bold text-center text-black md:text-5xl lg:text-5xl'>
+            Popular Categories
+          </h2>
+          <p className='mt-6 text-center text-black md:text-2xl lg:text-2xl'>
+            Latest Ad-free job board
+          </p>
         </div>
         <div className='w-full h-24'></div>
       </section>
