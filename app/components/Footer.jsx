55d3c02583c6e10dd8fe469f8d306df3233bcdb7
Harrison <jiksdevops@gmail.com>
2023-06-09 08:44:22 +0200

    Make footer sticky at the bottom

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Logo from "./atoms/Logo";
 const Footer = () => {
   return (
-    <div className='flex z-50 justify-center sticky bottom-0 py-2 align-middle bg-gray-900 h-15 md:h-20 lg:h-20 md:py-6 lg:py-6'>
+    <div className='flex z-50 justify-center bottom-0 py-2 align-middle bg-gray-900 h-15 md:h-20 lg:h-20 md:py-6 lg:py-6'>
       <div className='container flex justify-between align-middle md:px-3 lg:px-3'>
         <Logo />
         <p className='px-2'>Features coming soon</p>
