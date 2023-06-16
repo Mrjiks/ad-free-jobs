0f52e0b422d94fb3a80c666ca49cc46a7d7d76b2
Harrison <jiksdevops@gmail.com>
2023-06-16 23:53:41 +0200

    Add pagination to Job page

--- app/pages/jobs/page.js
+++ app/pages/jobs/page.js
@@ -2,18 +2,23 @@
 import getData from "@/app/lib/data/links";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
-import SingleJob from "./[jobId]/[url]";
 import { motion } from "framer-motion";
 
 const Jobs = () => {
   const url = "https://www.arbeitnow.com/api/job-board-api?page=1";
 
   const [jobList, setJobList] = useState([]);
+  const [page, setPage] = useState(1);
 
+  const handlePage = (selectedPage) => {
+    if (selectedPage >= 1 && selectedPage <= jobList.length / 10 && selectedPage !== page) {
+      setPage(selectedPage);
+    }
+  };
   const getJob = async () => {
     const { data } = await getData(url);
 
-    const sortedData = data.sort((a, b) => b.created_at - a.created_at).slice(1, 50);
+    const sortedData = data.sort((a, b) => b.created_at - a.created_at);
 
     setJobList(sortedData);
     return sortedData;
@@ -35,11 +40,33 @@ const Jobs = () => {
 
   return (
     <div className='container flex flex-col w-full min-h-screen mx-auto mb-2 overflow-hidden md:justify-between md:px-8 lg:px-20'>
-      <div>
-        <SingleJob />
+      <div className='flex flex-col items-center fixed inset-x-0 right-0 w-full mx-auto mb-10 bg-black h-[60px]'>
+        <p className='text-teal-900'> No of pages</p>
+        <div className='mx-auto bg-black'>
+          <span
+            onClick={() => handlePage(page - 1)}
+            className={page > 1 ? "cursor-pointer" : "opacity-0"}>
+            ⬅️
+          </span>
+          {[...Array(jobList.length / 10)].map((_, i) => {
+            return (
+              <span
+                key={i}
+                className={page === i + 1 ? "p-1 text-white" : "p-2 lg:mx-2"}
+                onClick={() => handlePage(i + 1)}>
+                {i + 1}
+              </span>
+            );
+          })}
+          <span
+            onClick={() => handlePage(page + 1)}
+            className={page < jobList.length / 10 ? "" : "opacity-0"}>
+            ➡️
+          </span>
+        </div>
       </div>
-      <div className='w-full'>
-        {jobList?.map((job) => {
+      <div className='w-full mt-20'>
+        {jobList?.slice(page * 10 - 10, page * 10).map((job) => {
           const { created_at, company_name, description, location, title, url, tags, slug } = job;
           const desc = description.replaceAll(/(<([^>]+)>)/gi, "+").split("+");
 
@@ -47,10 +74,10 @@ const Jobs = () => {
             <motion.div
               initial={{ opacity: 1 }}
               whileHover={{
-                scale: 1.02,
+                // scale: 1.01,
                 transition: { duration: 0.8 },
               }}
-              className='flex justify-center md:mt-2 md:w-full hover:translate-y-1 '
+              className='flex justify-center cursor-pointer md:mt-2 md:w-full'
               key={slug + description}>
               <div className='flex flex-col justify-between w-full h-auto mx-2 mb-8 align-middle bg-white border rounded-lg shadow-lg md:w-full md:flex-row lg:flex-row 2xl:flex-row'>
                 <div className='flex flex-col flex-wrap items-start w-full px-4 overflow-hidden text-black md:mr-3 md:p-4 md:w-1/2'>
@@ -79,10 +106,10 @@ const Jobs = () => {
                     Office Location: <span className='font-normal capitalize '>{location}</span>{" "}
                   </p>
                 </div>
-                <div className='flex items-center px-4 py-4'>
+                <div className='flex items-center px-4 py-4 mx-auto'>
                   <p className='p-2 bg-black border-blue-500 rounded-lg'>
                     <Link href={url}>
-                      Learn More and <span className='text-green-500'>Apply</span>
+                      View details and <span className='text-green-500'>Apply</span>
                     </Link>
                   </p>
                 </div>
